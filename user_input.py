601611b6c4d1182170d39c837e0e4c763dc24650
Asile Ayuba <asileayuba@gmail.com>
2024-10-31 08:24:40 +0100

    Completed week-1  assignment

---
+++ user_input.py
@@ -0,0 +1,10 @@
+# To collect input of user's name
+name = input("What is your name? ")
+
+# To collect input of user's age 
+age = input("How old are you? ")
+
+# To collect input of user's location
+location = input("Where do you live? ")
+
+print(f"Hello {name}, you are {age} years old and live in {location}.")
